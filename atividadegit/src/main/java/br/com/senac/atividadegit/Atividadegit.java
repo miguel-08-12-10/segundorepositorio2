fb6db62c32eafdf23169894f442e338d8bb9db49
Miguel duarte <ms1169929@gmail.com>
2026-08-31 16:15:45 -0300

    Criação do projeto de soma

---
+++ atividadegit/src/main/java/br/com/senac/atividadegit/Atividadegit.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.atividadegit;
+
+/**
+ *
+ * @author miguel62977036
+ */
+public class Atividadegit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int valor1;
+        int valor2;
+        int resultadofinal;
+        int soma;
+        
+        valor1 = 20;
+               
+        valor2 = 20;
+        
+        soma = (valor1 + valor2);
+        System.out.println("1 e 2 " +soma);
+    
+    }
+}
